ef5e3027b656a5cf1963b5f6fe08ebb033627f73
ADXM TECH <147929248+AdamKhelladi@users.noreply.github.com>
2024-02-06 07:49:32 +0100

    Update cars.py

--- cars.py
+++ cars.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def porsche_cars_info():
 
-  url = f"https://www.porsche.com/middle-east/_egypt_/models/?compare"
+  url = f"https://www.porsche.com/middle-east/_egypt_/models/?compare" 
  
   html = requests.get(url).text  
   soup = bs(html, "html.parser")  
